c051f9d33c8e111abccf842e29e400fa59eb119d
bardinGL <110034163+bardinGL@users.noreply.github.com>
2024-12-11 23:04:05 +0700

    Huhu

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import { Bounce, ToastContainer } from "react-toastify";
-//import Header from "./components/Header"; // Assume you have a Header component
 import AppRoutes from "./routes/AppRoutes";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      {/* Header */}
-      <Header />
-
-      {/* Banner Section */}
-      <div className="banner-container">
-        <img
-          src="/assets/banner.png"
-          alt="Banner"
-          className="banner-image"
-        />
-      </div>
-
-      {/* Main Content */}
       <div className="app-container">
         <AppRoutes />
       </div>
 
-      {/* Toast Notifications */}
       <ToastContainer
         position="top-right"
         autoClose={3000}
