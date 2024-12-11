cc9adfd93280a4d7b5d807d8b5770700dfa8349f
bardinGL <110034163+bardinGL@users.noreply.github.com>
2024-12-11 12:17:08 +0700

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ import "./styles/App.css";
 function App() {
   return (
     <>
+      {/* Add the banner image */}
+      <div className="banner-container">
+        <img
+          src="/assets/banner.png"
+          alt="Banner"
+          className="banner-image"
+        />
+      </div>
+
+      {/* Main App Content */}
       <div className="app-container">
         <AppRoutes />
       </div>
 
+      {/* Toast Notifications */}
       <ToastContainer
         position="top-right"
         autoClose={3000}
