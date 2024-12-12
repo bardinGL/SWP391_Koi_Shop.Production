835fdea34d60d65e43d22729301c38fc3b0e06fa
bardinGL <110034163+bardinGL@users.noreply.github.com>
2024-12-12 10:32:41 +0700

    ok

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
   };
 
   return (
-    <GoogleOAuthProvider clientId="684900073655-mu5vsdorjg8j82vkcf9uiuu7conm57fh.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="293260593150-g6s9uvapori7glal9uj0htvhqgu0i3am.apps.googleusercontent.com">
       <div className="login-container">
         <div className="back-arrow">
           <i
